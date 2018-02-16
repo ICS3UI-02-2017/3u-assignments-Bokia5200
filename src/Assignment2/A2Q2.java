6cdf67dfc9cb1ad8a0ff56ea73f73918ba5718dc
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-02-16 11:01:03 -0500

    A2Q2 finished

--- src/Assignment2/A2Q2.java
+++ src/Assignment2/A2Q2.java
@@ -6,7 +6,10 @@ package Assignment2;
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.IPredicate;
 import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /**
  *
@@ -18,11 +21,39 @@ public class A2Q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // create a city for the robot
         City bootstown = new City();
-        
+
         // create a robot
-        RobotSE monogram = new RobotSE(bootstown, 1, 2, Direction. EAST);
+        RobotSE monogram = new RobotSE(bootstown, 1, 2, Direction.EAST);
+
+        // create the floor
+        for (int i = 2; i <= 10; i = i + 1) {
+            new Wall(bootstown, 1, i, Direction.SOUTH);
+        }
+        // create hurdles
+        new Wall(bootstown, 1, 2, Direction.EAST);
+        new Wall(bootstown, 1, 3, Direction.EAST);
+        new Wall(bootstown, 1, 5, Direction.EAST);
+        new Wall(bootstown, 1, 8, Direction.EAST);
+
+        // create the finish line
+        new Thing(bootstown, 1, 10);
+
+        // make monogram complete the race
+        while (!monogram.canPickThing()){
+            if(!monogram.isBesideThing(IPredicate.aWall))
+                monogram.turnRight();
+        
+        if(monogram.frontIsClear()){
+            monogram.move();
+        }else{
+            monogram.turnLeft();
+        }
+        }      
+
+        
+
     }
 }
