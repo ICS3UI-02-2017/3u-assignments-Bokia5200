1972a1c51d13ab809e8bd163a12beb9a25a22267
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-02-21 10:34:17 -0500

    A2Q1 formatted

--- src/Assignment2/A2Q1.java
+++ src/Assignment2/A2Q1.java
@@ -19,13 +19,13 @@ public class A2Q1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // create a city
         City bootstown = new City();
-        
+
         // create a robot
         RobotSE monogram = new RobotSE(bootstown, 1, 1, Direction.EAST);
-        
+
         // place 10 things
         new Thing(bootstown, 1, 2);
         new Thing(bootstown, 1, 3);
@@ -37,12 +37,12 @@ public class A2Q1 {
         new Thing(bootstown, 1, 9);
         new Thing(bootstown, 1, 10);
         new Thing(bootstown, 1, 11);
-        
+
         // make the robot pick up 7 things, and move to the end of the row
-        while (monogram.countThingsInBackpack() < 7){
+        while (monogram.countThingsInBackpack() < 7) {
             monogram.move();
             monogram.pickThing();
         }
-            monogram.move(4);
+        monogram.move(4);
     }
 }
