d739ed235658d06b15b7f0f88b3ac76ffd7f40f1
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-02-22 11:16:55 -0500

    Challenge in Progress

--- src/Assignment2/Challenge.java
+++ src/Assignment2/Challenge.java
@@ -24,7 +24,7 @@ public class Challenge {
         City bootstown = new City();
 
         // create a robot
-        RobotSE monogram = new RobotSE(bootstown, 1, 3, Direction.SOUTH);
+        RobotSE monogram = new RobotSE(bootstown, 0, 3, Direction.SOUTH);
 
         // place road
         for (int i = 0; i <= 10; i = i + 1) {
@@ -33,5 +33,50 @@ public class Challenge {
         for (int i = 0; i <= 10; i = i + 1) {
             new Wall(bootstown, i, 2, Direction.EAST);
         }
+        // create side of sidewalk
+        new Wall(bootstown, 0 , 3, Direction.EAST);
+                
+        // create first driveway
+        new Wall(bootstown, 1, 4, Direction.NORTH);
+        new Wall(bootstown, 1, 5, Direction.NORTH);
+        new Wall(bootstown, 1, 6, Direction.NORTH);
+        new Wall(bootstown, 1, 7, Direction.NORTH);
+        new Wall(bootstown, 2, 4, Direction.SOUTH);
+        new Wall(bootstown, 2, 5, Direction.SOUTH);
+        new Wall(bootstown, 2, 6, Direction.SOUTH);
+        new Wall(bootstown, 2, 7, Direction.SOUTH);
+        new Wall(bootstown, 1, 7, Direction.EAST);
+        new Wall(bootstown, 2, 7, Direction.EAST);
+        
+        // Make monogram shovel one part of the driveway at a time
+        if (monogram.canPickThing()) {
+            monogram.move();
+            monogram.turnLeft();
+        }
+        while (true) {
+            while (monogram.frontIsClear()) {
+                monogram.move();
+                while (monogram.canPickThing()) {
+                    monogram.pickThing();
+                }
+            }
+            monogram.turnAround();
+            while (monogram.frontIsClear()) {
+                monogram.move();
+            }
+            monogram.putAllThings();
+            monogram.turnLeft();
+            monogram.move();
+            if (monogram.canPickThing()) {
+                monogram.turnLeft();
+            } else {
+                monogram.move();
+                monogram.turnLeft();
+                while (!monogram.frontIsClear()) {
+                    monogram.turnRight();
+                }
+            }
+        }
+
     }
 }
