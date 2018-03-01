1d9ce303ec328c1952272c62caa23934978e438f
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-03-01 11:17:35 -0500

    A4Q4 in progress

---
+++ src/Assignment4/A4Q4.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A4Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // use a scanner to read the info, ask the user for all the expenses, and store each value
+        Scanner input = new Scanner(System.in);
+        System.out.print("How much does the food for prom cost? :");
+        int food = input.nextInt();
+        System.out.print("How much does the DJ cost? :");
+        int DJ = input.nextInt();
+        System.out.print("How much does it cost to rent the hall? :");
+        int hall = input.nextInt();
+    }
+}
