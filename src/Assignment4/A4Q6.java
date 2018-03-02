0c34fa02cedbb52a3a9b15a348cb5bf00243d729
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-03-02 11:14:47 -0500

    A4Q6 in progress

---
+++ src/Assignment4/A4Q6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // use a scanner to read the info
+    }
+}
