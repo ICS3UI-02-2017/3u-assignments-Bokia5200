5ce4e50181cb2d2da58e016a1882e1b88b9ec997
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-03-02 10:59:23 -0500

    A4Q5 finished

--- src/Assignment4/A4Q5.java
+++ src/Assignment4/A4Q5.java
@@ -21,40 +21,41 @@ public class A4Q5 {
         System.out.print("Please enter your name:");
         String name = input.nextLine();
         System.out.print("What was the first test out of?:");
-        int total1 = input.nextInt();
+        double total1 = input.nextDouble();
         System.out.print("What mark did you get?:");
-        int mark1 = input.nextInt();
+        double mark1 = input.nextDouble();
         System.out.print("What was the second test out of?:");
-        int total2 = input.nextInt();
+        double total2 = input.nextDouble();
         System.out.print("What mark did you get?:");
-        int mark2 = input.nextInt();
+        double mark2 = input.nextDouble();
         System.out.print("What was the third test out of?:");
-        int total3 = input.nextInt();
+        double total3 = input.nextDouble();
         System.out.print("What mark did you get?:");
-        int mark3 = input.nextInt();
+        double mark3 = input.nextDouble();
         System.out.print("What was the fourth test out of?:");
-        int total4 = input.nextInt();
+        double total4 = input.nextDouble();
         System.out.print("What mark did you get?:");
-        int mark4 = input.nextInt();
+        double mark4 = input.nextDouble();
         System.out.print("What was the fifth test out of?:");
-        int total5 = input.nextInt();
+        double total5 = input.nextDouble();
         System.out.print("What mark did you get?:");
-        int mark5 = input.nextInt();
+        double mark5 = input.nextDouble();
 
         // Calculate the marks for each test, and the average for all 5 tests
 
         System.out.println("Test scores for Doug");
-        double test1 = mark1 / total1;
+        double test1 = mark1 / total1 * 100;
         System.out.println("test1: " + test1 + "%");
-        double test2 = mark2 / total2;
+        double test2 = mark2 / total2 * 100;
         System.out.println("test2: " + test2 + "%");
-        double test3 = mark3 / total3;
+        double test3 = mark3 / total3 * 100;
         System.out.println("test3: " + test3 + "%");
-        double test4 = mark4 / total4;
+        double test4 = mark4 / total4 * 100;
         System.out.println("test4: " + test4 + "%");
-        double test5 = mark5 / total5;
+        double test5 = mark5 / total5 * 100;
         System.out.println("test5: " + test5 + "%");
-        double avg = test1 + test2 + test3 + test4 + test5;
+        double total = test1 + test2 + test3 + test4 + test5;
+        double avg = total/5;
         System.out.println("Average: " + avg + "%");
 
     }
