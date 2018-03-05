72f9b8322fed31b805cfd222770b706e106978fc
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-03-05 11:18:04 -0500

    A4Q6 Finished

--- src/Assignment4/A4Q6.java
+++ src/Assignment4/A4Q6.java
@@ -4,6 +4,8 @@
  */
 package Assignment4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bokia5200
@@ -15,6 +17,26 @@ public class A4Q6 {
      */
     public static void main(String[] args) {
         
-        // use a scanner to read the info
+        // use a scanner to read the info, and ask the user to input the information
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Enter the speed limit:");
+        int limit = input.nextInt();
+        System.out.print("Enter the recorded speed of the car:");
+        int speed = input.nextInt();
+        
+        // give feedback on speed
+        if(speed < limit){
+            System.out.println("Congratulations, you are within the speed limit");
+        }
+        if(speed >= limit + 1 && speed <= limit + 20) {
+            System.out.println("You are speeding and your fine is $100.");
+        }
+        if(speed >= limit + 21 && speed <= limit + 30) {
+            System.out.println("You are speeding and your fine is $270");
+        }
+        if (speed >= limit + 31){
+            System.out.println("You are speeding and your fine is $500");
+        }
     }
 }
