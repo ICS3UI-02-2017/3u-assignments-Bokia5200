4a750c92d9b1a5aacb6cd576ecfe1fb1604fddce
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-03-20 11:16:34 -0400

    A5Q1 in progress

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -27,29 +27,29 @@ public class A5Q1 {
 
             // Sanitize the input
             origWord = origWord.toLowerCase();
-            
+
             // Use a for loop to go through each character, looking for a vowel
             int length = origWord.length();
             String transWord = "";
-            for (int i = 0; i < length; i++){
-                if (origWord.charAt(i) == 'a' || origWord.charAt(i) == 'e' || origWord.charAt(i) == 'i' || origWord.charAt(i) == 'o' || origWord.charAt(i) == 'u'){
+            for (int i = 0; i < length; i++) {
+                if (origWord.charAt(i) == 'a' || origWord.charAt(i) == 'e' || origWord.charAt(i) == 'i' || origWord.charAt(i) == 'o' || origWord.charAt(i) == 'u') {
                     String start = origWord.substring(0, i);
                     String end = origWord.substring(i);
-                    
+
                     // Compute translated word
                     transWord = start + "ub" + end;
                 }
             }
-            for (int i = 0; i < length; i++){
-                if (transWord.charAt(i) == 'a' || transWord.charAt(i) == 'e' || transWord.charAt(i) == 'i' || transWord.charAt(i) == 'o' || transWord.charAt(i) == 'u'){
+            for (int i = 0; i < length; i++) {
+                if (transWord.charAt(i) == 'a' || transWord.charAt(i) == 'e' || transWord.charAt(i) == 'i' || transWord.charAt(i) == 'o' || transWord.charAt(i) == 'u') {
                     String start = transWord.substring(0, i);
                     String end = transWord.substring(i);
-                    
+
                     // Compute translated word
                     String transWord1 = start + "ub" + end;
-            }
-            System.out.println(origWord + " in Ubbi Dubbi is: " + transWord1);
+                }
+                System.out.println(origWord + " in Ubbi Dubbi is: " + transWord);
             }
         }
     }
-
+}
