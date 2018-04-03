b18286470526c19eb9f9210ad46c362cdede4c42
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-03 11:08:35 -0400

    A6Q1 in progress

---
+++ src/Assignment6/A6Q1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user for the number of students in the class
+        System.out.println("How many students are in the class? ");
+        int students = input.nextInt();
+
+        // Calculate the sum of all the marks
+        int[] results = new int[students];
+        int sum = 0;
+        System.out.println("Enter the marks: ");
+        for (int i = 0; i < results.length; i++) {
+            int marks = input.nextInt();
+            sum = sum + marks + results[i];
+        }
+        // Calculate the average of the test results, and output it to the user
+        double avg = sum / students;
+        System.out.println("The class average is: " + avg + "%");
+    }
+}
+
