7aead79abe70e592b25cc0298cfbbde5a92fa378
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-04 11:17:28 -0400

    A6Q3 in progress

---
+++ src/Assignment6/A6Q3.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner (System.in);
+        
+        // Tell the user to write two numbers
+        System.out.println("Enter two integers:  ");
+        int numbers = input.nextInt();
+        
+        // Sort the numbers in ascending order
+        int[] num = new int[2];
+    }
+}
