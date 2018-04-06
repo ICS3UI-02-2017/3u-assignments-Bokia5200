6b1089d722e54515e405c4819a68d44e983291f8
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-06 11:16:51 -0400

    A6Q3 finished

--- src/Assignment6/A6Q3.java
+++ src/Assignment6/A6Q3.java
@@ -16,28 +16,27 @@ public class A6Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner input = new Scanner (System.in);
-        
+
+        Scanner input = new Scanner(System.in);
+
         // Tell the user to write two numbers
         System.out.println("Enter two integers:  ");
-        
+
         // Create an array for the numbers to be inputted into
         int[] num = new int[2];
         for (int i = 0; i < num.length; i++) {
             int numbers = input.nextInt();
-            num [i] = numbers;
+            num[i] = numbers;
         }
-        for (int i = 0; i < num.length; i++) {
-            // If the number is less than the number ahead of it, output it before the other number
-            if (num [i] < num [i + 1]) {
-                System.out.println("The integers in ascending order are: " + num [i] + " " + num [i + 1]);
-            } else {
-                // If the number is greater than the number ahead of it, output if after the other number
-                System.out.println("The integers in ascending order are: " + num [i + 1] + " " + num [i]);
-            }
-            break;
+        // If the number is less than the number ahead of it, output it before the other number
+        if (num[1] < num[0]) {
+            int num1 = num[0];
+            num[0] = num[1];
+            num[1] = num1;
+            System.out.println("The integers in ascending order are: " + num[0] + " " + num[1]);
+        } else {
+            // If the number is greater than the number ahead of it, output if after the other number
+            System.out.println("The integers in ascending order are: " + num[0] + " " + num[1]);
         }
-        
     }
 }
