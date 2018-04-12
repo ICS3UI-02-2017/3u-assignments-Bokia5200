6e69faa72cb94895368803c0d1037f1fac342a27
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-12 10:27:17 -0400

    A6Q4 finished

--- src/Assignment6/A6Q4.java
+++ src/Assignment6/A6Q4.java
@@ -5,7 +5,7 @@
 package Assignment6;
 
 import java.util.Scanner;
-
+import java.util.Arrays;
 /**
  *
  * @author bokia5200
@@ -22,21 +22,20 @@ public class A6Q4 {
         // Tell the user to input ten numbers
         System.out.println("Enter ten marks: ");
         // Create an array for the ten numbers to be inputted into
-        int[] marks = new int[11];
-        
-        for (int i = 0; i < marks.length - 1; i++) {
+        int[] marks = new int[10];
+        for (int i = 0; i < marks.length; i++) {
             int numbers = input.nextInt();
             marks[i] = numbers;
+        }
+        for (int i = 0; i < marks.length - 1; i++) {
             for (int j = i + 1; j < marks.length; j++) {
                 if (marks[i] > marks[j]) {
                     int numT = marks[i];
                     marks[i] = marks[j];
                     marks[j] = numT; 
                 }
-            }   
+            }
         }
-        for (int i = 0; i < marks.length - 1; i++) {
-            
-        }
+            System.out.println("The marks in ascending order are: " + Arrays.toString(marks));
     }
 }
