a5c56557767d46b11560d3e535916f5852b808a0
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-17 11:15:15 -0400

    A6Q7 in progress

---
+++ src/Assignment6/A6Q7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment6;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
