ec799492aa05b67052c11f4088bf5ef96c8a82a7
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-18 11:04:41 -0400

    A6Q7 in progress

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -4,6 +4,8 @@
  */
 package Assignment6;
 
+import java.util.Arrays;
+
 /**
  *
  * @author bokia5200
@@ -15,6 +17,29 @@ public class A6Q7 {
      */
     public static void main(String[] args) {
         
-        
+        // Create an array and input all the numbers from 2 to 1000
+        int[] numbers = new int[999];
+        int p = 2;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = p;
+            p = p + 1;
+        }
+        System.out.println(Arrays.toString(numbers));
+//        for (int i = 0; i < numbers.length; i++) {
+//            boolean isPrime = true;
+//            
+//            // Check to see if the numbers are prime
+//            for (int j = 2; j < i; j++) {
+//                if (i%j==0) {
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            // Print the number
+//            if (isPrime) {
+//                 System.out.println(i);
+//            }
+//            
+//        }
     }
 }
