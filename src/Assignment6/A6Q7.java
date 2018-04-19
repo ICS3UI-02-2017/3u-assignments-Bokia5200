9e132761d53b7f682e7ec5e3533f854b8235d0d7
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-19 11:06:09 -0400

    A6Q7 in progress

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -16,30 +16,60 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // Create an array and input all the numbers from 2 to 1000
         int[] numbers = new int[999];
-        int p = 2;
+        int x = 2;
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = p;
-            p = p + 1;
+            numbers[i] = x;
+            x += 1;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                numbers[i] = 0;
+                numbers[0] = 2;
+            }
+            if (numbers[i] % 3 == 0) {
+                numbers[i] = 0;
+                numbers[1] = 3;
+            }
+            if (numbers[i] % 5 == 0) {
+                numbers[i] = 0;
+                numbers[3] = 5;
+            }
+            if (numbers[i] % 7 == 0) {
+                numbers[i] = 0;
+                numbers[5] = 7;
+            }
+            if (numbers[i] % 9 == 0) {
+                numbers[i] = 0;
+            }
+            if (numbers[i] % 11 == 0) {
+                numbers[i] = 0;
+                numbers[9] = 11;
+            }
+            if (numbers[i] % 13 == 0) {
+                numbers[i] = 0;
+                numbers[11] = 13;
+            }
+        }
+        int[] prime = new int[999];
+        for (int i = 0; i < prime.length; i++) {
+            if (numbers[i] != 0) {
+                numbers[i] = prime[i];
+            }
         }
         System.out.println(Arrays.toString(numbers));
-//        for (int i = 0; i < numbers.length; i++) {
-//            boolean isPrime = true;
-//            
-//            // Check to see if the numbers are prime
-//            for (int j = 2; j < i; j++) {
-//                if (i%j==0) {
-//                    isPrime = false;
-//                    break;
+        //        for (int i = 0; i < numbers.length; i++) {
+//            for (int p = 2; p < numbers.length; p++) {
+//                if (numbers[i] % p == 0) {
+//                    boolean isPrime = false;
+//                    numbers[i] = 0;
+//                    numbers[0] = 2;
+//                } else if (numbers[i] % p != 0) {
+//                    boolean isPrime = true;
 //                }
 //            }
-//            // Print the number
-//            if (isPrime) {
-//                 System.out.println(i);
-//            }
-//            
 //        }
     }
 }
