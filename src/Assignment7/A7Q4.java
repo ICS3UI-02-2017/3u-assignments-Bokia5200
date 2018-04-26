3bc3a5019d4b7ea9ee879abfd82eb26c51099878
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-26 10:31:50 -0400

    A7Q4 finished

--- src/Assignment7/A7Q4.java
+++ src/Assignment7/A7Q4.java
@@ -12,20 +12,26 @@ import java.util.Scanner;
  */
 public class A7Q4 {
 
-    public static double compoundInterest(double principle, double rate, int time) {
+    public static double compoundInterest(double p, double r, int n) {
+        
         // Calculate the compound interest
-        double B = principle * ((1 + rate)* Math.pow(principle, time));
+        double B = p * (Math.pow((1+r), n));
         return B;
     }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
-         System.out.println("What is the principal amount, the interest rate, and the number of years?");
-         double p = input.nextDouble();
-         double r = input.nextDouble();
-         double n = input.nextDouble();
-         
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the principal amount, the interest rate, and the number of years?");
+        double p = input.nextDouble();
+        double r = input.nextDouble();
+        int n = input.nextInt();
+        // Output the result to the user
+        double B = compoundInterest(p,r,n);
+        
+        System.out.println("The balance remaining is: $" + B);
+        
     }
 }
