a57558a2ec8982a10ae9b950179f3d41328695c8
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-04-30 10:38:25 -0400

    A7Q6 in progress

---
+++ src/Assignment7/A7Q6.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bokia5200
+ */
+public class A7Q6 {
+
+    public static int lastDigit (int n) {
+        int b = 0;
+        for (int i = 0; i < 10; i++) {
+            if (n % 10 == i) {
+                b = i;   
+            }   
+        }
+        return b;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int digit = lastDigit(input.nextInt());
+        System.out.println("The last digit of your number is: " + digit);
+    }
+}
