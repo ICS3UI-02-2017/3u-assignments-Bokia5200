69ba6bf79d4c6e1a7e100705680e63641473ec3e
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-05-02 10:41:23 -0400

    A7Q8 Done

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -14,15 +14,15 @@ public class A7Q8 {
 
     public static boolean allDigitsOdd (int n) {
         // Set the initial number to true
-        boolean odd = true;
+        boolean number = true;
         // While the number is greater than 10 or lesser than -10, see if it can be divided by two evenly, otherwise divide by ten
         while(n >= 10 || n <= -10){
             if (n % 2 == 0) {
-                odd = false;
+                number = false;
             }
             n /= 10;
         }
-        return odd;
+        return number;
     }
     /**
      * @param args the command line arguments
@@ -32,8 +32,8 @@ public class A7Q8 {
         Scanner input = new Scanner(System.in);
         // Ask the user to input a number
         System.out.print("Enter a number: ");
-        boolean number = allDigitsOdd(input.nextInt());
+        boolean oddOrEven = allDigitsOdd(input.nextInt());
         // Tell the user whether all the digits in their number are odd
-        System.out.println("All the digits in your number are odd: " + number);
+        System.out.println("All the digits in your number are odd: " + oddOrEven);
     }
 }
