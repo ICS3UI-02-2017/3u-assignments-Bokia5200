a952b0f77f9bb30c5ab9f8b03b94bc429e79333e
bokia5200 <bokia5200@HRH-IC0027329.wrdsb.ca>
2018-05-10 10:49:23 -0400

    A7Q4 Done

--- src/Assignment7/A7Q4.java
+++ src/Assignment7/A7Q4.java
@@ -24,13 +24,13 @@ public class A7Q4 {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Ask for the three values
         System.out.println("What is the principal amount, the interest rate, and the number of years?");
         double p = input.nextDouble();
         double r = input.nextDouble();
         int n = input.nextInt();
         // Output the result to the user
         double B = compoundInterest(p,r,n);
-        
         System.out.println("The balance remaining is: $" + B);
         
     }
